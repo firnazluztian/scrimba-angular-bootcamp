208c3b020f0cf5ee300d06e6903562708663991f
Firnaz Luztian <firnazluztian@gmail.com>
2020-04-03 14:58:27 +0700

    add customers, footer, header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers/customers.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
